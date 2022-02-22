e275e36f5abef110b2576ed8e45d2ef94c157757
Erik Stryshak <estryshak@gmail.com>
2022-02-21 19:55:11 -0600

    write json file with timestamp

--- data_pull.py
+++ data_pull.py
@@ -1,28 +1,38 @@
 import pandas as pd
 import requests
 import json
+from datetime import datetime
 
 # get ynab_token from local file
-ynab_token_df = pd.read_csv("ynab_token.txt", header=None)
+ynab_token_df = pd.read_csv('ynab_token.txt', header=None)
 ynab_token = ynab_token_df.iloc[0][0]
 
 # get last knowledge of server variable from local file
-last_knowledge_df = pd.read_csv("budget_last_knowledge_of_server.csv")
+last_knowledge_df = pd.read_csv('budget_last_knowledge_of_server.csv')
 last_knowledge = last_knowledge_df.iloc[-1, 0]
 
-my_headers = {"Authorization" : 'Bearer {}'.format(ynab_token)}
-base_url = "https://api.youneedabudget.com/v1"
+my_headers = {'Authorization' : 'Bearer {}'.format(ynab_token)}
+base_url = 'https://api.youneedabudget.com/v1'
 
 
 # get budget from api
-budget_id = "9edc28cf-7cf4-40ea-84d4-2865cd23a66a"
-budget_url = "/budgets/{}".format(budget_id)
-last_knowledge_url ="?last_knowledge_of_server={}".format(last_knowledge)
+budget_id = '9edc28cf-7cf4-40ea-84d4-2865cd23a66a'
+budget_url = '/budgets/{}'.format(budget_id)
+last_knowledge_url ='?last_knowledge_of_server={}'.format(last_knowledge)
 budget_response = requests.get(base_url + budget_url, headers=my_headers)
 budget_json = budget_response.json()
 
 
 # write new last_knowledge_of_server value to file
-with open("budget_last_knowledge_of_server.csv", "a") as fd:
-    fd.write("\n" + str(budget_json["data"]["server_knowledge"]))
+with open('budget_last_knowledge_of_server.csv', 'a') as fd:
+    fd.write('\n' + str(budget_json['data']['server_knowledge']))
 
+
+# write new json file to directory
+cur_time = datetime.now().strftime('%Y%m%d-%H%M%S')
+with open('budget-' + cur_time + '.json', 'w') as f:
+    json.dump(budget_json, f, ensure_ascii=False)
+
+# add data pull time to file
+with open('last_data_pull.csv', 'a') as fd:
+    fd.write('\n' + cur_time)
